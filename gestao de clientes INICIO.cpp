377e7ef81c3dd40e1eac7d2bceb7e63afbaf6eb7
moises lima <moiseskeko65@gmail.com>
2023-06-02 12:17:34 -0300

    PROGURSSO DO CADASTRO DE CLIENTES

--- gestao de clientes INICIO.cpp
+++ gestao de clientes INICIO.cpp
@@ -3,7 +3,9 @@
 #include <locale.h>
 #include <ctype.h>
 
-#define MAX_CLIENTES 150
+#define MAX_CLIENTES 200
+
+void desenho();
 
 struct Endereco {
 	int numero;
@@ -12,6 +14,8 @@ struct Endereco {
 	char bairro[30];
 	char cidade[40];
 	char UF[2];
+	char pais[20];
+
 };
 
 struct Cliente {
@@ -20,13 +24,16 @@ struct Cliente {
 	int idade;
 	int telefone;
 	int idDoCliente;
+	int datadenascimento[10];
+	char email[50];
+	int id;
 	Endereco endereco;
 };
 
 int menu(Cliente * clientes);
 
 Cliente clientes[MAX_CLIENTES];
-int totalClientes = 0;
+int totalclientes = 0;
 
 int main() {
 	setlocale(LC_ALL, "portuguese");
@@ -72,8 +79,11 @@ void escolha(int opicao1, Cliente *clientes){
 	
 	switch (opicao1)
 	    case 1:
-	    	printf("========## CADASTRO DE CLIENTES ##=======");
-	    	
+	    	printf("=======================================/n")
+			printf("========## CADASTRO DE CLIENTES ##=======");
+	    	printf("=========================================/n")
+			
+			cadastro(Cliente *clientes,  int *numclientes)
 	    	
 	    	break;
 	    case 2:
@@ -107,8 +117,14 @@ void cabecario(){
 
 }
 
-void cadastro(Cliente *clientes){
+void cadastro(Cliente *clientes,  int *numclientes){
 	char opicao
+	if (*numClientes < MAX_CLIENTES){
+	Cliente clientes;
+	
+	fflush(stdin);
+	printf("\t=====- ID DO CLIENTE (NÚMEROS) -=====: ");
+	gets(clientes[code].id);
 	
 	fflush(stdin);
 	printf("\t=====- NOME -=====: ");
@@ -122,11 +138,88 @@ void cadastro(Cliente *clientes){
 	printf("\t=====- IDADE -=====: ");
 	gets(clientes[code].idade);
 	
-	printf("\t\t\t=====================================================\n");
-	printf("\t\t\t======= CADASTRAR ENDEREÇLO =======\n");
-	printf("\t\t\t=====================================================\n"
+	fflash(stdin);
+	printf("\t=====- DATA DE NASCIMENTO (dd/mm/aaaa) -=====: ");
+	gets(clientes[code].datadenascimento);
 	
+	fflush(stdin);
+	printf("\t=====- EMAIL -=====: ");
+	gets(clientes[code].email);
 	
+	fflush(stdin);
+	printf("\t=====- NÚMERO TELEFONICO ( (75) 91234-5678 ) -=====: ");
+	gets(clientes[code].telefone);
+	
+	desenho();
+	
+	printf("\n\t\t\t=====================================================\n");
+	printf("\t\t\t======= CADASTRAR ENDEREÇO =======\n");
+	printf("\t\t\t=====================================================\n\n"
+	
+	printf("====- LOGRADOURO -====: ");
+	scanf("%s", &clientes[code].endereco.logradouro);
+	
+	fflush(stdin);
+	printf("====- NÚMERO DA CASA -====: ");
+	scanf("%d", &clentes[code].endereco.numero);
+	
+	fflush(stdin);
+	printf("====- CEP -====: ");
+	scanf("%d", &clentes[code].endereco.CEP);
+		
+	fflush(stdin);
+	printf("====- BAIRRO -====: ");
+	scanf("%s", &clentes[code].endereco.bairro);
+		
+	fflush(stdin);
+	printf("====- CIDADE -====: ");
+	scanf("%s", &clentes[code].endereco.cidade);
+		
+	fflush(stdin);
+	printf("====- UF -====: ");
+	scanf("%s", &clentes[code].endereco.UF);
+	
+	Cliente[*numClientes] = clientes;
+        (*numClientes)++;
+        
+        printf("==============================================================/n");
+        printf("=====- OBRIGADO O CLIENTE FOI CADASTRADO COM SUCESSO -=====!\n");
+        printf("==============================================================/n");
+    } else {
+        printf("==============================================================/n");
+		printf("====- INFELIZMENTE O NUMERO DE CLIENTES FOI OBTIDO -====.\n");
+		printf("==============================================================/n");
+
+    }
 	
 }
+	
+void listadeclientes(Cliente *clientes){
+	char op;
+
+
+	if (totalclientes == 0) {
+        printf("\t\t\t==========- INFELIZMENTE NENHUM CLIENTE FOI CADASTRADO -=========.\n");
+        return;
+    }
+    
+    printf("==============================================================/n");
+    printf("===================- LISTA DE TODOS OS CLIENTES -=============!\n");
+    printf("==============================================================/n");
+
+    for(int i = 0; i < code; i++) {
+		printf("\n");
+		printf("%d - %s\n", i, clientes[i].nome);
+	}
+	
+	fflush(stdin);
+	printf("\t\t\t=======- MAIS INFORMAÇOES? -========= (S/N): ");
+	scanf("%c", &op);
+	
+	system("cls");
+     
+
+}
+	
+
 

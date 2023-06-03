8f63e2753602a6a0c3c553c777ba8958b29ec3b6
Iannzit0 <134064638+iannzit0@users.noreply.github.com>
2023-06-03 08:58:26 -0300

    Update gestao de clientes INICIO.cpp

--- gestao de clientes INICIO.cpp
+++ gestao de clientes INICIO.cpp
@@ -49,27 +49,30 @@ void escolha(int opicao1, Cliente *clientes){
 	
 	switch(opicao1){
 	    case 1:
-	    	printf("========## CADASTRO DE CLIENTES ##=======");
+	    	printf("========## CADASTRO DE CLIENTES ##=======\n");
 	    	
 	    	
 	    	break;
 	    case 2:
-		    printf("=========** LISTA DE CLIENTES CADASTRADOS **=========");
+		    printf("=========** LISTA DE CLIENTES CADASTRADOS **=========\n");
 			
 			
 			break;
 		case 3:
-		   printf("=========$$ PESQUISAR CLIENTES POR ID $$========");
+		   printf("=========$$ PESQUISAR CLIENTES POR ID $$========\n");
 		   
 		   
 		   break;
 		case 0:
-		   printf("========== O SEU SISTEMA FOI ENCERRADO COM SUCESSO!!!! ==========");
+		   printf("========== O SEU SISTEMA FOI ENCERRADO COM SUCESSO!!!! ==========\n");
+		   exit(0);
 		   
 		   break;
 		default:
-		    printf("*** DESCULPE, A OPIÇÃO ESCOLHIDA ESTA INVALIDA!! ***");
+		    printf("*** DESCULPE, A OPÇÃO ESCOLHIDA É INVALIDA!! ***");
 			printf("*** TENTE NOVAMENTE OUTRA OPÇÃO ***");
+			break;
+			
 	}
     }
 
@@ -90,7 +93,7 @@ int menu(Cliente * clientes){
     printf("\t\t\t\t=================================================\n\n\n\n");
 	printf("\n\n\n===== SEJA BEM-VINDO A NOSSA CENTRAL DE CADASTRO DE CLIENTES =====");
 	
-	printf("\t1 - -= cadastrar cliente =-\n");
+	printf("\n\n\t1 - -= cadastrar cliente =-\n");
     printf("\t2 - -= lista de todos os clientes cadastrados =-\n");
     printf("\t3 - -= Pesquisar cliente por ID =-\n");
     printf("\t0 - -= Sair =-\n\n");
@@ -129,4 +132,3 @@ void cadastro(Cliente *clientes){
 	
 	
 }
-
